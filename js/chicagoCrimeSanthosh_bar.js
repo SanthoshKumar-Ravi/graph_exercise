03072c8ef3149a1ec680ddec67e7a7e92118e09a
SanthoshKumar_Ravi <krpsanthoshkumar@gmail.com>
2017-05-10 10:58:56 +0530

    after indentation

--- js/chicagoCrimeSanthosh_bar.js
+++ js/chicagoCrimeSanthosh_bar.js
@@ -3,11 +3,12 @@ module.exports  =  function convert(startYear)
 {
 const readline  =  require('readline');
 const fs  =  require('fs');
-
-  if(typeof startYear  =  =  =  'string') {
+let log4js = require('log4js');
+let logger = log4js.getLogger();
+  if(typeof startYear  ===  'string') {
     return "";
   }
-  if(typeof startYear ! =  =  'number' || isNaN(startYear)) {
+  if(typeof startYear !==  'number' || isNaN(startYear)) {
   throw new Error('Not a number');
 }else{
 var rl  =  readline.createInterface({
@@ -24,11 +25,11 @@ var theft_over_2001   =   0, theft_under_2001  =  0,
     theft_over_2002 = 0, theft_under_2002  =  0, theft_over_2003 = 0, theft_under_2003 = 0,
     theft_over_2004 = 0, theft_under_2004 = 0, theft_over_2005 = 0, theft_under_2005 = 0,
     theft_over_2006 = 0, theft_under_2006 = 0, theft_over_2008 = 0, theft_under_2008 = 0,
-    theft_over_2007 = 0,theft_under_2007 = 0,theft_over_2009 = 0,theft_under_2009 = 0,
-    theft_over_2010 = 0,theft_under_2010 = 0,theft_over_2011 = 0,theft_under_2011 = 0,
-    theft_over_2012 = 0,theft_under_2012 = 0,
-    theft_over_2013 = 0,theft_under_2013 = 0,theft_over_2014 = 0,theft_under_2014 = 0,
-    theft_over_2015 = 0,theft_under_2015 = 0,theft_over_2016 = 0,theft_under_2016 = 0;
+    theft_over_2007 = 0, theft_under_2007 = 0, theft_over_2009 = 0, theft_under_2009 = 0,
+    theft_over_2010 = 0, theft_under_2010 = 0, theft_over_2011 = 0, theft_under_2011 = 0,
+    theft_over_2012 = 0, theft_under_2012 = 0,
+    theft_over_2013 = 0, theft_under_2013 = 0, theft_over_2014 = 0, theft_under_2014 = 0,
+    theft_over_2015 = 0, theft_under_2015 = 0, theft_over_2016 = 0, theft_under_2016 = 0;
 
  rl.on('line', function (data) {
   if(i =  = 0){
@@ -39,8 +40,8 @@ var theft_over_2001   =   0, theft_under_2001  =  0,
   }
   
    var result  =  [];
-   //console.log(data);
-   //console.log(data);
+   //logger.debug(data);
+   //logger.debug(data);
    data_1 = data.split(',');
 var j1 = data_1[date_index].substring(6,10);
 yr_theft.push({year:j1,desc:data_1[theft_index]});
@@ -49,204 +50,203 @@ yr_theft.push({year:j1,desc:data_1[theft_index]});
 
  function hi(yr_theft)
 {
-  //console.log(yr_theft);
+  //logger.debug(yr_theft);
   for(i = 0;i<yr_theft.length;i++)
   {
-  if(yr_theft[i].year =  = '2001')
+  if(yr_theft[i].year === '2001')
    {
     
-    if(yr_theft[i].desc =  = 'OVER $500')
+    if(yr_theft[i].desc === 'OVER $500')
    {
         theft_over_2001++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2001++;
    }
  }
 
 
-   if(yr_theft[i].year =  = '2002')
+   if(yr_theft[i].year === '2002')
    {
-   if(yr_theft[i].desc =  = 'OVER $500')
+   if(yr_theft[i].desc === 'OVER $500')
    {
         theft_over_2002++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2002++;
    }}
    
-   if(yr_theft[i].year =  = '2003')
+   if(yr_theft[i].year === '2003')
    {
-   if(yr_theft[i].desc =  = 'OVER $500')
+   if(yr_theft[i].desc === 'OVER $500')
    {
     
     theft_over_2003++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2003++;
    }}
 
-   if(yr_theft[i].year =  = '2004')
+   if(yr_theft[i].year === '2004')
    {
-   if(yr_theft[i].desc =  = 'OVER $500')
+   if(yr_theft[i].desc === 'OVER $500')
    {
     
     theft_over_2004++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2004++;
    }}
 
-   if(yr_theft[i].year =  = '2005')
+   if(yr_theft[i].year === '2005')
    {
-   if(yr_theft[i].desc =  = 'OVER $500')
+   if(yr_theft[i].desc === 'OVER $500')
    {
     
     theft_over_2005++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2005++;
    }}
 
-   if(yr_theft[i].year =  = '2006')
+   if(yr_theft[i].year === '2006')
    {
-   if(yr_theft[i].desc =  = 'OVER $500')
+   if(yr_theft[i].desc === 'OVER $500')
    {
     
     theft_over_2006++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2006++;
    }}
 
 
-   if(yr_theft[i].year =  = '2007')
+   if(yr_theft[i].year === '2007')
    {
-   if(yr_theft[i].desc =  = 'OVER $500')
+   if(yr_theft[i].desc === 'OVER $500')
    {
     
     theft_over_2007++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2007++;
    }}
 
-   if(yr_theft[i].year =  = '2008')
+   if(yr_theft[i].year === '2008')
    {
-   if(yr_theft[i].desc =  = 'OVER $500')
+   if(yr_theft[i].desc === 'OVER $500')
    {
     
     theft_over_2008++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2008++;
    }}
 
-   if(yr_theft[i].year =  = '2009')
+   if(yr_theft[i].year === '2009')
    {
-   if(yr_theft[i].desc =  = 'OVER $500')
+   if(yr_theft[i].desc === 'OVER $500')
    {
     
     theft_over_2009++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2009++;
    }}
 
-   if(yr_theft[i].year =  = '2010')
+   if(yr_theft[i].year === '2010')
    {
    if(yr_theft[i].desc =  = 'OVER $500')
    {
     
     theft_over_2010++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2010++;
    }}
 
-   if(yr_theft[i].year =  = '2011')
+   if(yr_theft[i].year === '2011')
    {
-   if(yr_theft[i].desc =  = 'OVER $500')
+   if(yr_theft[i].desc === 'OVER $500')
    {
     
     theft_over_2011++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2011++;
    }}
 
-   if(yr_theft[i].year =  = '2012')
+   if(yr_theft[i].year === '2012')
    {
-   if(yr_theft[i].desc =  = 'OVER $500')
+   if(yr_theft[i].desc === 'OVER $500')
    {
-    
     theft_over_2012++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2012++;
    }}
 
-   if(yr_theft[i].year =  = '2013')
+   if(yr_theft[i].year === '2013')
    {
-   if(yr_theft[i].desc =  = 'OVER $500')
+   if(yr_theft[i].desc === 'OVER $500')
    {
     
     theft_over_2013++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2013++;
    }}
 
-   if(yr_theft[i].year =  = '2014')
+   if(yr_theft[i].year === '2014')
    {
-   if(yr_theft[i].desc =  = 'OVER $500')
+   if(yr_theft[i].desc === 'OVER $500')
    {
     
     theft_over_2014++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2014++;
    }}
 
-   if(yr_theft[i].year =  = '2015')
+   if(yr_theft[i].year === '2015')
    {
-   if(yr_theft[i].desc =  = 'OVER $500')
+   if(yr_theft[i].desc === 'OVER $500')
    {
     
     theft_over_2015++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2015++;
    }}
 
-   if(yr_theft[i].year =  = '2016')
+   if(yr_theft[i].year === '2016')
    {
-   if(yr_theft[i].desc =  = 'OVER $500')
+   if(yr_theft[i].desc === 'OVER $500')
    {
     
     theft_over_2016++;
    }
-   if(yr_theft[i].desc =  = '$500 AND UNDER'){
+   if(yr_theft[i].desc === '$500 AND UNDER'){
     theft_under_2016++;
    }}
  }
 
- console.log("in 2001 "+theft_under_2001+" "+theft_over_2001);
- console.log("in 2002 "+theft_under_2002+" "+theft_over_2002);
- console.log("in 2003 "+theft_under_2003+" "+theft_over_2003);
- console.log("in 2004 "+theft_under_2004+" "+theft_over_2004);
- console.log("in 2005 "+theft_under_2005+" "+theft_over_2005);
- console.log("in 2006 "+theft_under_2006+" "+theft_over_2006);
- console.log("in 2007 "+theft_under_2007+" "+theft_over_2007);
- console.log("in 2008"+theft_under_2008+" "+theft_over_2008);
- console.log("in 2009 "+theft_under_2009+" "+theft_over_2009);
- console.log("in 2010 "+theft_under_2010+" "+theft_over_2010);
- console.log("in 2011 "+theft_under_2011+" "+theft_over_2011);
- console.log("in 2012 "+theft_under_2012+" "+theft_over_2012);
- console.log("in 2013 "+theft_under_2013+" "+theft_over_2013);
- console.log("in 2014 "+theft_under_2014+" "+theft_over_2014);
- console.log("in 2015 "+theft_under_2015+" "+theft_over_2015);
- console.log("in 2016 "+theft_under_2016+" "+theft_over_2016);
+ logger.debug("in 2001 "+theft_under_2001+" "+theft_over_2001);
+ logger.debug("in 2002 "+theft_under_2002+" "+theft_over_2002);
+ logger.debug("in 2003 "+theft_under_2003+" "+theft_over_2003);
+ logger.debug("in 2004 "+theft_under_2004+" "+theft_over_2004);
+ logger.debug("in 2005 "+theft_under_2005+" "+theft_over_2005);
+ logger.debug("in 2006 "+theft_under_2006+" "+theft_over_2006);
+ logger.debug("in 2007 "+theft_under_2007+" "+theft_over_2007);
+ logger.debug("in 2008"+theft_under_2008+" "+theft_over_2008);
+ logger.debug("in 2009 "+theft_under_2009+" "+theft_over_2009);
+ logger.debug("in 2010 "+theft_under_2010+" "+theft_over_2010);
+ logger.debug("in 2011 "+theft_under_2011+" "+theft_over_2011);
+ logger.debug("in 2012 "+theft_under_2012+" "+theft_over_2012);
+ logger.debug("in 2013 "+theft_under_2013+" "+theft_over_2013);
+ logger.debug("in 2014 "+theft_under_2014+" "+theft_over_2014);
+ logger.debug("in 2015 "+theft_under_2015+" "+theft_over_2015);
+ logger.debug("in 2016 "+theft_under_2016+" "+theft_over_2016);
 
 /*
  for(i = 1;i< = 16;i++)
